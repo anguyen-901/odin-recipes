2d16b6a518a3d6cd0d46019b7b5ea98dbdb983b7
Albert Nguyen <albertnguyen34@gmail.com>
2024-04-19 22:18:06 -0500

    added button to prompt for gridSize

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const grid = document.createElement('div');
-grid.classList.add('grid');
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    });
-    square.addEventListener('mouseleave', (e) => {
-        e.target.style.backgroundColor = "";
-    });
-    grid.appendChild(square);
-  }
-}
+let gridSize;
+const gridSizeBtn = document.getElementById('gridSize');
 
-// Add the grid to the document
-document.body.appendChild(grid);
+gridSizeBtn.addEventListener("click", function() {
+  const userInput = prompt('Please enter the size of the grid: ');
+  if (userInput) {
+    gridSize = userInput;
+  } else {
+    gridSize = 16;
+  }
+  const grid = document.createElement('div');
+  grid.classList.add('grid');
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.addEventListener('mouseover', (e) => {
+          e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+      });
+      square.addEventListener('mouseleave', (e) => {
+          e.target.style.backgroundColor = "";
+      });
+      grid.appendChild(square);
+    }
+  }
+  // Add the grid to the document
+  document.body.appendChild(grid);
+});
+
